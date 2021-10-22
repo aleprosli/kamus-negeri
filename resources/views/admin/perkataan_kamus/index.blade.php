213e1d2477ff2e01413c164a4391e38461f34ddd
Aliff <aliff.rosli96@gmail.com>
2021-10-22 15:18:06 +0800

    index blade perkataan kamus

---
+++ resources/views/admin/perkataan_kamus/index.blade.php
@@ -0,0 +1,42 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Perkataan Kamus Yang Ada</div>
+
+                <div class="card-body">
+                  
+                  <a href="{{ route('perkataan_kamus:create') }}" type="button" class="btn btn-primary">Tambah Perkataan</a>
+                  <h1>Senarai perkataan</h1>
+                  <table class="table">
+                    <thead>
+                      <tr>
+                        <th scope="col">Nombor</th>
+                        <th scope="col">Perkataan</th>
+                        <th scope="col">Maksud Ayat</th>
+                        <th scope="col">Contoh Ayat</th>
+                        <th scope="col">Maksud Ayat</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      @foreach ( $semuaperkataankamus as $key => $perkataan)
+                        <tr>
+                          <th scope="row">{{ $key + 1 }}</th>
+                          <td>{{ $perkataan->perkataan }}</td>
+                          <td>{{ $perkataan->maksud_perkataan }}</td>
+                          <td>{{ $perkataan->contoh_ayat }}</td>
+                          <td>{{ $perkataan->maksud_ayat }}</td>
+                        </tr>
+                      @endforeach
+                    </tbody>
+                  </table>
+                </div>
+            </div>
+
+        </div>
+    </div>
+</div>
+@endsection
