5826184015995450687174d67abb57c085f16d7a
Aliff <aliff.rosli96@gmail.com>
2021-10-22 15:29:06 +0800

    route updated

--- routes/web.php
+++ routes/web.php
@@ -24,5 +24,5 @@ Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name
 
 Route::get('/perkataan-kamus/index', [App\Http\Controllers\Admin\PerkataanKamusController::class, 'index'])->name('perkataan_kamus:index');
 Route::get('/perkataan-kamus/create', [App\Http\Controllers\Admin\PerkataanKamusController::class, 'create'])->name('perkataan_kamus:create');
-Route::get('/perkataan-kamus/edit', [App\Http\Controllers\Admin\PerkataanKamusController::class, 'edit'])->name('perkataan_kamus:edit');
+Route::get('/perkataan-kamus/edit/{perkataan}', [App\Http\Controllers\Admin\PerkataanKamusController::class, 'edit'])->name('perkataan_kamus:edit');
 Route::post('/perkataan-kamus/store', [App\Http\Controllers\Admin\PerkataanKamusController::class, 'store'])->name('perkataan_kamus:store');
