5a452226d54c5e1891c4b2f32227182b41bd58a1
Aliff <aliff.rosli96@gmail.com>
2021-10-22 15:19:02 +0800

    Function index create store added , compact index

---
+++ app/Http/Controllers/Admin/PerkataanKamusController.php
@@ -0,0 +1,99 @@
+<?php
+
+namespace App\Http\Controllers\Admin;
+
+use App\Models\Perkataan;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class PerkataanKamusController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $semuaperkataankamus = Perkataan::all();
+        return view('admin.perkataan_kamus.index',compact('semuaperkataankamus'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        return view('admin.perkataan_kamus.create');
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //store perkataan guna relationship - model
+        $perkataankamus = new Perkataan();
+        
+        $perkataankamus->perkataan = $request->perkataan;
+        $perkataankamus->maksud_perkataan = $request->maksud_perkataan;
+        $perkataankamus->contoh_ayat = $request->contoh_ayat;
+        $perkataankamus->maksud_ayat = $request->maksud_ayat;
+
+        //save
+        $perkataankamus->save();
+
+        return redirect()->route('perkataan_kamus:index');
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit()
+    {
+        return view('admin.perkataan_kamus.edit');
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+
+}
